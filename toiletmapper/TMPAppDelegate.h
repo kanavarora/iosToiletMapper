e845379d398a9f33316aeebc1d374ab60da348c6
Arora <kanav@pocketgems.com>
2014-02-28 05:53:16 -0800

    Initial Commit

---
+++ toiletmapper/TMPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TMPAppDelegate.h
+//  toiletmapper
+//
+//  Created by Kanav Arora on 2/28/14.
+//  Copyright (c) 2014 Kanav Arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ toiletmapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  toiletmapper
+//
+//  Created by Kanav Arora on 2/28/14.
+//  Copyright (c) 2014 Kanav Arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMPAppDelegate class]));
+    }
+}
